538c86aa999f49d4e413c83975be309d9e718da1
ruzgargulucafe <ugurcangull@icloud.com>
2026-08-28 11:13:25 +0300

    Add Firebase Authentication to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js"; // ✅ EKLENDİ
 
 const firebaseConfig = {
   apiKey: "AIzaSyCj4F_8WOwLzVoREs-gRZDXfgYEkLtNvig",
@@ -12,7 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app); // ✅ EKLENDİ
 
-export { db, storage };
+export { db, storage, auth }; // ✅ EKLENDİ
