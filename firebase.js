9ac095b689c52b9b6108c5809f11c139adadc417
ruzgargulucafe <ugurcangull@icloud.com>
2026-08-29 12:21:56 +0300

    Implement user login functionality with Firebase
    Updated Firebase authentication logic to handle user login.

--- firebase.js
+++ firebase.js
@@ -1,21 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js"; // ✅ EKLENDİ
+import { auth } from "./firebase.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCj4F_8WOwLzVoREs-gRZDXfgYEkLtNvig",
-  authDomain: "ruzgarguluqr.firebaseapp.com",
-  projectId: "ruzgarguluqr",
-  storageBucket: "ruzgarguluqr.firebasestorage.app",
-  messagingSenderId: "1034312304751",
-  appId: "1:1034312304751:web:3b0e15ba06a937455a659a"
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+document.getElementById("loginBtn").onclick = async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Email ve şifre gir!");
+    return;
+  }
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("Giriş başarılı ✅");
+    window.location.href = "admin.html";
+
+  } catch (error) {
+    console.log(error);
+    alert(error.code); // 👈 NET hata gör
+  }
+
 };
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-const storage = getStorage(app);
-const auth = getAuth(app); // ✅ EKLENDİ
-
-export { db, storage, auth }; // ✅ EKLENDİ
