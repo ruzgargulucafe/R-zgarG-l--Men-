62e8f0bf983290a73448a85f4167f5f3e6ddf274
ruzgargulucafe <ugurcangull@icloud.com>
2026-08-30 12:14:14 +0300

    Refactor adminAuth for Firebase initialization and control
    Updated Firebase imports and modified authentication redirection logic.

--- adminAuth.js
+++ adminAuth.js
@@ -1,14 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
   onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const auth = getAuth();
+// 🔥 CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCj4F_8WOwLzVoREs-gRZDXfgYEkLtNvig",
+  authDomain: "ruzgarguluqr.firebaseapp.com",
+  projectId: "ruzgarguluqr"
+};
 
+// 🔥 INIT
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// 🔐 AUTH KONTROL
 onAuthStateChanged(auth, (user) => {
 
   if (!user) {
-    window.location.href = "login.html";
+    window.location.href = "index.html"; // login sayfan
+    return;
+  }
+
+  // 🔥 ADMIN KONTROL
+  if (user.email !== "caferuzgargulu@gmail.com") {
+    alert("Yetkisiz kullanıcı");
+    window.location.href = "index.html";
   }
 
 });
